4b2e0f23f10dd92b3939a62921ef3f0f7854e27f
jackpetersen3 <peterja7@oregonstate.edu>
2021-06-04 12:59:27 -0700

    add fnctionality and test created

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+import example
+
+class TestCase(unittest.TestCase):
+
+    def test_add_1(self):
+        self.assertEqual(example.add(2,3), 5)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
